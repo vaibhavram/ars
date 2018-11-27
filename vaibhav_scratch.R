4658c93d68c3616eee569c604d930ad084ecf8a9
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2018-11-27 15:53:53 -0800

    finished draft of U function

--- vaibhav_scratch.R
+++ vaibhav_scratch.R
@@ -3,11 +3,10 @@
 library(numDeriv)
 
 g <- function(x) {
-  return(-2 * x^2 + 100)
+  return(-1*x^2 + 100)
 }
 
 grad(g, 1:3)
-
 
 #### getting u's
 x <- -5:5
@@ -17,12 +16,31 @@ h <- function(x) {
   return(log(g(x)))
 }
 
-get_u_j <- function(j, x, z, h) {
-  
+D <- c(-10, 10)
+
+get_u_j <- function(j, x, z, h, D) {
+  u <- function(t) {
+    if (j == 0) {
+      xj <- D[1]
+    } else if (j == len(x)) {
+      xj <- D[2]
+    } else {
+      xj <- x[j]
+    }
+    h_xj <- h(xj)
+    h_prime_xj <- grad(h, xj)
+    a <- h_xj - xj*h_prime_xj
+    b <- h_prime_xj
+    return(a+b*t)
+  }
+  return(u)
 }
 
+get_u <- function(x, z, h, D) {
+  return(lapply(1:length(x), get_u_j, x, z, h, D))
+}
 
-
+u <- get_u(x, z, h, D)
 
 
 
