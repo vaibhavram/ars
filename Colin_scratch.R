ae512571d43f43b9f817d8272da3af82568de296
zhiwei-kou <42983921+zhiwei-kou@users.noreply.github.com>
2018-11-27 17:20:41 -0800

    A random number generator

---
+++ Colin_scratch.R
@@ -0,0 +1,41 @@
+library(numDeriv)
+
+# we assume that the input function is log concave
+random_num_generator <- function(fun, k=3){
+
+  # randomly pick a number between 0 and 1
+  x <- runif(1)
+
+  # if its first derivative is negative
+  if ( (grad(fun,x)/fun(x)) < 0 ) {
+    min = x
+    # find the upper bound iteratively
+    while( (grad(fun,x)/fun(x)) <= 0 ){
+      x <- runif(1, x, x+1)
+    }
+    max = x
+  }
+  # if its first derivative is zero or positive
+  else{
+    if( (grad(fun,x)/fun(x)) == 0 ){
+    x = x+1
+    }
+    max = x
+    # find the upper bound iteratively
+    while( (grad(fun,x)/fun(x)) >= 0){
+      x <- runif(1, x-1, x)
+    }
+    min = x
+  }
+
+  m = k-2
+  nums <- runif(m, min, max)
+  output <- c(min,sort(nums),max)
+  
+  return(output)
+}
+
+# test with the pdf of exponential
+set.seed(0)
+f <- dexp
+test_output <- random_num_generator(f, k=10)
