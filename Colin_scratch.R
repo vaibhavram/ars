92172f94d2c9fb16880e81e1de8485917d5c0ed4
zhiwei-kou <42983921+zhiwei-kou@users.noreply.github.com>
2018-11-27 17:58:45 -0800

    updated random number generator

--- Colin_scratch.R
+++ Colin_scratch.R
@@ -5,27 +5,26 @@ random_num_generator <- function(fun, k=3){
 
   # randomly pick a number between 0 and 1
   x <- runif(1)
-
   # if its first derivative is negative
   if ( (grad(fun,x)/fun(x)) < 0 ) {
-    min = x
+    max = x
     # find the upper bound iteratively
     while( (grad(fun,x)/fun(x)) <= 0 ){
-      x <- runif(1, x, x+1)
+      x <- runif(1, x-1, x)
     }
-    max = x
+    min = x
   }
   # if its first derivative is zero or positive
   else{
     if( (grad(fun,x)/fun(x)) == 0 ){
     x = x+1
     }
-    max = x
+    min = x
     # find the upper bound iteratively
     while( (grad(fun,x)/fun(x)) >= 0){
-      x <- runif(1, x-1, x)
+      x <- runif(1, x, x+1)
     }
-    min = x
+    max = x
   }
 
   m = k-2
@@ -37,5 +36,5 @@ random_num_generator <- function(fun, k=3){
 
 # test with the pdf of exponential
 set.seed(0)
-f <- dexp
+f <- dnorm
 test_output <- random_num_generator(f, k=10)
