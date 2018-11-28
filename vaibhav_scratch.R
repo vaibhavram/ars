966e9fc30e3b5047236e30691aaa049e05e4ce22
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2018-11-27 19:50:14 -0800

    got sampling to work

--- vaibhav_scratch.R
+++ vaibhav_scratch.R
@@ -2,38 +2,35 @@
 
 library(numDeriv)
 
-g <- function(x) {
-  return(-1*x^2 + 100)
-}
-
-grad(g, 1:3)
+# g <- function(x) {
+#   return(-1*x^2 + 100)
+# }
+# 
+# grad(g, 1:3)
 
 #### getting u's
-x <- -5:5
-z <- seq(-4.5, 4.5, by = 1)
+x <- -3:3
+z <- seq(-2.5, 2.5, by = 1)
 
 h <- function(x) {
-  return(2*x - 10*log(1 + exp(x)) - 0.5*x^2 + 100)
+  return(2*x - 10*log(1 + exp(x)) - 0.5*x^2 + 50)
 }
 
-D <- c(-10, 10)
+D <- c(-5, 4)
 
+# param j: index of the u piece to return
+# param x: vector of x points
+# param h: log of density function
+# param D: domain of density function
+# return: slope and intercept of tangent line at x[j]
 get_u_piece <- function(j, x, h, D) {
-  u <- function(t) {
-    if (j == 0) {
-      xj <- D[1]
-    } else if (j == length(x)) {
-      xj <- D[2]
-    } else {
-      xj <- x[j]
-    }
-    h_xj <- h(xj)
-    h_prime_xj <- grad(h, xj)
-    a <- h_xj - xj*h_prime_xj
-    b <- h_prime_xj
-    return(a+b*t)
-  }
-  return(u)
+  # throw error if j is 0 or more than k
+  xj <- x[j]
+  h_xj <- h(xj)
+  h_prime_xj <- grad(h, xj)
+  intercept <- h_xj - xj*h_prime_xj
+  slope <- h_prime_xj
+  return(list(intercept = intercept, slope = slope))
 }
 
 get_u <- function(x, h, D) {
@@ -42,33 +39,42 @@ get_u <- function(x, h, D) {
 
 u <- get_u(x, h, D)
 
-get_u_integral <- function(u, z, D) {
+get_u_integral <- function(u, x, z, D) {
   full_z <- c(D[1], z, D[2])
-  full_integral <- 0
-  integrals <- sapply(1:length(u), function(j) {fun <- u[[j]]; (fun(full_z[j]) + fun(full_z[j+1]))/2})
-  # for (j in 1:length(u)) {
-  #   fun <- u[[j]]
-  #   integral <- (fun(full_z[j]) + fun(full_z[j+1]))/2
-  #   print(paste0(as.character(j), ": ", as.character(integral)))
-  #   full_integral <- full_integral + integral
-  # }
+  
+  get_integral <- function(j) {
+    fun <- function(t) {u[[j]]$intercept + u[[j]]$slope*t}
+    return ((fun(full_z[j]) + fun(full_z[j+1]))/2)*(x[j+1] - x[j])
+  }
+  
+  integrals <- sapply(1:length(u), get_integral)
+  
   return(integrals)
 }
 
-u_integrals <- get_u_integral(u, z, D)
+u_integrals <- get_u_integral(u, x, z, D)
 
-# get_s <- function(u, z, D) {
-#   u_integrals <- get_u_integral(u, z, D)
-#   s <- lapply(1:length(u), function(t) {})
-# }
-
-sample.s <- function(n, u, z, D) {
-  u_integrals <- get_u_integral(u, z, D)
+# general gist: use CDF of u to sample from s
+sample.s <- function(n, u, x, z, D) {
+  full_z <- c(D[1], z, D[2])
+  u_integrals <- get_u_integral(u, x, z, D)
   s_integrals <- u_integrals/sum(u_integrals)
-  cumsum_s <- cumsum(s_integrals)
-  q_vec <- runif(n)
+  cumsum_s <- c(0, cumsum(s_integrals))
+  q <- runif(1)
   
-  for (q in q_vec)
+  j <- max(which(q > cumsum_s))
+  spillover <- q - cumsum_s[j]
+  u_star <- u[[j]]
+  z_val <- full_z[j]
+  next_z <- full_z[j+1]
+  
+  # preparing quadratic formula
+  a <- 0.5*u_star$slope
+  b <- u_star$intercept
+  c <- -(spillover*sum(u_integrals) + u_star$intercept*z_val + 0.5*u_star$slope*z_val^2)
+  candidates <- c((-b+sqrt(b^2-4*a*c))/(2*a), (-b-sqrt(b^2-4*a*c))/(2*a))
+  sample <- candidates[which(candidates < next_z & candidates > z_val)]
+  return(sample)
 }
 
 
