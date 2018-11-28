7ac3e70ca446349cf2c3b3e27ab8219d16581b31
Vaibhav Ramamoorthy <vaibhavr@berkeley.edu>
2018-11-27 18:48:00 -0800

    working on S

--- vaibhav_scratch.R
+++ vaibhav_scratch.R
@@ -13,12 +13,12 @@ x <- -5:5
 z <- seq(-4.5, 4.5, by = 1)
 
 h <- function(x) {
-  return(log(g(x)))
+  return(2*x - 10*log(1 + exp(x)) - 0.5*x^2 + 100)
 }
 
 D <- c(-10, 10)
 
-get_u_j <- function(j, x, z, h, D) {
+get_u_piece <- function(j, x, h, D) {
   u <- function(t) {
     if (j == 0) {
       xj <- D[1]
@@ -36,13 +36,40 @@ get_u_j <- function(j, x, z, h, D) {
   return(u)
 }
 
-get_u <- function(x, z, h, D) {
-  return(lapply(1:length(x), get_u_j, x, z, h, D))
+get_u <- function(x, h, D) {
+  return(lapply(1:length(x), get_u_piece, x, h, D))
 }
 
-u <- get_u(x, z, h, D)
+u <- get_u(x, h, D)
 
+get_u_integral <- function(u, z, D) {
+  full_z <- c(D[1], z, D[2])
+  full_integral <- 0
+  integrals <- sapply(1:length(u), function(j) {fun <- u[[j]]; (fun(full_z[j]) + fun(full_z[j+1]))/2})
+  # for (j in 1:length(u)) {
+  #   fun <- u[[j]]
+  #   integral <- (fun(full_z[j]) + fun(full_z[j+1]))/2
+  #   print(paste0(as.character(j), ": ", as.character(integral)))
+  #   full_integral <- full_integral + integral
+  # }
+  return(integrals)
+}
 
+u_integrals <- get_u_integral(u, z, D)
+
+# get_s <- function(u, z, D) {
+#   u_integrals <- get_u_integral(u, z, D)
+#   s <- lapply(1:length(u), function(t) {})
+# }
+
+sample.s <- function(n, u, z, D) {
+  u_integrals <- get_u_integral(u, z, D)
+  s_integrals <- u_integrals/sum(u_integrals)
+  cumsum_s <- cumsum(s_integrals)
+  q_vec <- runif(n)
+  
+  for (q in q_vec)
+}
 
 
 
